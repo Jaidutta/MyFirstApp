f3282cbd07cef1149939a2fafb8d670ce59e0f0a
Joy Dutta <dutta_joy@hotmail.com>
2023-12-20 11:00:49 +0000

    04. Query String

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -3,12 +3,15 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    string path = context.Request.Path;
-    string method = context.Request.Method;
-
     context.Response.Headers["Content-type"] = "text/html";
-    await context.Response.WriteAsync($"<p>{path}</p>");
-    await context.Response.WriteAsync($"<p>{method}</p>");
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
 });
 
 app.Run();
